7f1031f6f4543223dbdd7583797044c8ac5f37c1
Maryiam Hassan <mmaryamhassann1@gmail.com>
2025-06-30 00:32:49 +0500

    Initial commit for QuickNotes

---
+++ tailwind.config.js
@@ -0,0 +1,9 @@
+// tailwind.config.js
+module.exports = {
+  content: ["./*.html"],  // Or adjust based on your file structure
+  darkMode: 'class',      // ← Add this line!
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
